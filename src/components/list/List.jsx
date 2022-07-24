d6e4b4055f6cbbdbe69f5dd0917c52f942fee869
Minji Jeon <minji799@gmail.com>
2022-07-24 21:38:58 +0900

    done문제 해결완료

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,33 +2,33 @@ import React, { Fragment, useState } from "react";
 import "./style.css";
 import Todo from "../todo/Todo";
 
-
-
-
 const List = (props) => {
   let todoList = props.todoList;
   let setTodoList = props.setTodoList;
-
-
+  let [todoDone, setTodoDone] = useState([
+    { id: 5, title: "시험보기", body: "알고리즘시험" },
+  ]);
   // const deleteTodo = (id) => props.setTodo(todoList.filter((x) => x.id !== id));
   return (
     <>
       <h2 className="working">working</h2>
       <hr />
       <div className="listBox">
-       <Todo todoList={todoList}
-       setTodoList={setTodoList}></Todo>
+  
+        <Todo todoList={todoList}
+         todoDone={todoDone}
+         setTodoDone={setTodoDone}
+          setTodoList={setTodoList} done></Todo>
       </div>
 
-
-      
       <h2 className="working">done</h2>
-     <hr />
+      <hr />
       <div className="listBox">
-       
-      <Todo todoList={todoList}
-       setTodoList={setTodoList}></Todo>
-        
+    
+            <Todo todoList={todoList} 
+             todoDone={todoDone}
+             setTodoDone={setTodoDone}
+             setTodoList={setTodoList}></Todo>
       </div>
     </>
   );
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,24 +1,38 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./style.css";
 
-const Todo = ({ todoList, setTodoList }) => {
+const Todo = ({ todoList, setTodoList, done, todoDone,setTodoDone}) => {
   //완료
-  let [todoDone, setTodoDone] = useState([]);
-  let [done, setDone] = useState(false);
-
-  const [state, setState] = React.useState(todoList);
-
-  React.useEffect(() => {
-    setState(todoList);
-  }, [todoList]);
-
-  //done ? done : working
+  
   return (
     <>
-      ///false알때
+      {done ? (
+        <Working
+          done={done}
+          todoList={todoList}
+          setTodoList={setTodoList}
+          todoDone={todoDone}
+          setTodoDone={setTodoDone}
+        />
+      ) : (
+        <Done
+          done={done}
+          todoList={todoList}
+          setTodoList={setTodoList}
+          todoDone={todoDone}
+          setTodoDone={setTodoDone}
+        />
+      )}
+    </>
+  );
+};
+
+function Working({ todoList, setTodoList, todoDone, setTodoDone }) {
+  return (
+    <>
       {todoList.map((x, i) => {
         return (
-          <Fragment key={x.id}>
+          <Fragment key={x.title}>
             <div className="box">
               <div>
                 <h3>{x.title}</h3>
@@ -44,12 +58,12 @@ const Todo = ({ todoList, setTodoList }) => {
                     }); //기본 배열에서 지우고
                     setTodoList(test);
                     let test2 = todoList.filter((y) => {
-                      return y.id == x.id;
+                      return y.title == x.title;
                     }); //완료만 따로뺌
-                    console.log(test2);
                     let copy = [...todoDone];
-
                     setTodoDone([...todoDone, ...test2]);
+                    console.log("완료");
+                    console.log(test2);
                     console.log(todoDone);
                   }}
                 >
@@ -61,7 +75,13 @@ const Todo = ({ todoList, setTodoList }) => {
           </Fragment>
         );
       })}
-      /////////////////////////////
+    </>
+  );
+}
+
+function Done({ todoList, setTodoList, todoDone, setTodoDone }) {
+  return (
+    <>
       {todoDone.map((x, i) => {
         return (
           <Fragment key={x.title}>
@@ -75,7 +95,7 @@ const Todo = ({ todoList, setTodoList }) => {
                   className="btn"
                   onClick={() => {
                     let test = todoDone.filter((y) => {
-                      return y.id !== x.id;
+                      return y.title !== x.title;
                     });
                     setTodoDone(test);
                     console.log(test);
@@ -87,12 +107,12 @@ const Todo = ({ todoList, setTodoList }) => {
                   className="btn"
                   onClick={() => {
                     let test = todoDone.filter((y) => {
-                      return y.id !== x.id;
-                    }); // todoDone 배열에서 지우고
+                      return y.title !== x.title;
+                    });
                     setTodoDone(test);
                     let test2 = todoDone.filter((y) => {
-                      return y.id == x.id;
-                    }); //취소는 다시  todoList로
+                      return y.title == x.title;
+                    });
                     setTodoList([...test2, ...todoList]);
                     console.log(test2);
                   }}
@@ -104,9 +124,7 @@ const Todo = ({ todoList, setTodoList }) => {
           </Fragment>
         );
       })}
-      ///////////////////////
     </>
   );
-};
-
+}
 export default Todo;
